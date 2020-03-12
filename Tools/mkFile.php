b943635e29f212d352eb69cf05f987191c9764c9
NiuJunQing <junqing.niu@leapoon.com>
2020-03-12 18:25:39 +0800

    update:新建文件

---
+++ Tools/mkFile.php
@@ -0,0 +1,15 @@
+<?php
+
+function createFile($sFileName, $iLoops = 500)
+{
+	for($i = 1; $i <= $iLoops; $i++)
+	{
+		$sStr = '#### Q'. $i . '.' . PHP_EOL . PHP_EOL . PHP_EOL;
+		file_put_contents($sFileName, $sStr, FILE_APPEND);
+	}
+
+	echo 'success';
+}
+
+
+createFile('./面试题总汇-MySQL.md');
